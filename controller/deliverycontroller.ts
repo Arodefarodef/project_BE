d30b8ae7c7f5097ce73601ffe69a61debe372384
Arodefarodef <umudefedora@gmail.com>
2024-06-14 12:53:29 -0700

    contoller

---
+++ controller/deliverycontroller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, request } from "express";
+import deliveryModel from "../model/deliverymodel";
+
+export const createDeliveryGuy = async (req: Request, res: Response) => {
+  try {
+    const { FullName, email, phone } = req.body;
+    const getD = await deliveryModel.create({ Fname, Lname, email, phone });
+    res.status(200).json({
+      message: "Delivery Guy Created!",
+      status: 200,
+      data: getD,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Error Occured",
+      status: 400,
+      error: error.message,
+    });
+  }
+};
--- model/deliverymodel.ts
+++ model/deliverymodel.ts
@@ -1,35 +1,34 @@
 import { Schema, Types, model } from "mongoose";
 import { ideliveryData } from "../utils/types";
 
-
-const  deliverymodel = new Schema<ideliveryData>(
-    {
-        fullName: {
-            type: String,
-        },
-        userame: {
-            type: String,
-        },
-        password: {
-            type: String,
-        },
-        email: {
-            type: String,
-        },
-        address: {
-            type: String,
-        },
-        phonenumber: {
-            type: String,
-        },
-        images: [
-            {
-                type: Types.ObjectId,
-                ref: "images",
-            },
-        ],
+const deliverymodel = new Schema<ideliveryData>(
+  {
+    fullName: {
+      type: String,
     },
-    { timestamps: true}
+    userame: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    phonenumber: {
+      type: String,
+    },
+    images: [
+      {
+        type: Types.ObjectId,
+        ref: "images",
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 export default model("agents", deliverymodel);

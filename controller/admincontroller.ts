4ce93635afe1727d3281488af17966ce90849201
Arodefarodef <umudefedora@gmail.com>
2024-06-16 11:19:54 -0700

    updated again

--- controller/admincontroller.ts
+++ controller/admincontroller.ts
@@ -51,40 +51,35 @@ export const verifyUser = async (req: Request, res: Response) => {
   }
 };
 
-export const signinUser = async (req: Request, res: Response) => {
+export const viewOne = async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
-    const userExist = await adminModel.findOne({ email });
-    const salt = await bcrypt.genSalt(10);
-    const hashed = await bcrypt.hash(password, salt);
-    if (userExist) {
-      if (userExist.verify) {
-        const passCheck = await bcrypt.compare(password, hashed);
-        if (passCheck) {
-          res.status(200).json({
-            message: "Logged in Successfully",
-            data: userExist,
-            status: 200,
-          });
-        } else {
-          res.status(400).json({
-            message: "Password Incorrect",
-            status: 400,
-          });
-        }
-      } else {
-        res.status(400).json({
-          message: "User not Verified yet",
-          status: 400,
-        });
-      }
-    } else {
-      res.status(400).json({
-        message: "User doesn't exist",
-        status: 400,
-      });
-    }
-  } catch (error) {
-    res.status(400);
+    const { Id } = req.params;
+    const getD = await deliverymodel.findById(Id);
+    res.status(200).json({
+      message: "User Found",
+      data: getD,
+      status: 200,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      message: "Error Occured",
+      status: 400,
+    });
+  }
+};
+
+export const viewAll = async (req: Request, res: Response) => {
+  try {
+    const getD = await adminModel.find();
+    res.status(200).json({
+      message: "All Users",
+      status: 200,
+      data: getD,
+    });
+  } catch (error: any) {
+    res.status(200).json({
+      message: "Error Occured",
+      status: 400,
+    });
   }
 };
